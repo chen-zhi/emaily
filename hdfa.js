b5b22739ca8cae17d99d6296580f2533e1ffdda4
Zhi Chen <chenzhi.finland@gmail.com>
2018-02-26 16:31:57 +0200

    add google oauth

---
+++ hdfa.js
@@ -0,0 +1,5 @@
+https://accounts.google.com/o/oauth2/v2/auth?
+response_type=code&
+redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
+scope=profile%20email
+&client_id=989361599214-6ctep4o7g12374k3og19jehadtgahg6l.apps.googleusercontent.com
